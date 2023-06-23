3b734ed892a8b32060940b0a5b9309badffa3187
Nikita Levshin <levshinno@yandex.ru>
2023-06-23 22:11:49 +0300

    refactor: добавлены аннотации utilityclass для мапперов, убраны аннотации data для сущностей, добавить обработку исключений, добавить аннотации транзакции для методов, меняющих данные в БД

--- src/main/java/ru/practicum/shareit/booking/model/Booker.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booker.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.booking.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
